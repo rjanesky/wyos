fa452950d4e4082e410b243221d3c792f9960069
rjanesky <rjanesky@easternmetal.com>
2019-10-16 13:45:28 -0400

    Began refactoring application,  and restarted chapter 3.

--- app/Main.hs
+++ app/Main.hs
@@ -4,54 +4,26 @@ import Lib
 
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
-import Control.Applicative hiding ((<|>), many)
 
-data LispValue = Atom String
-    | List [LispValue]
-    | DottedList [LispValue] LispValue
-    | Number Integer
-    | String String
-    | Bool Bool
-    deriving (Show)
+main :: IO ()
+main = do
+    putStr "=> "
+    input <- getLine
+    if input == "exit" 
+        then 
+            putStrLn "Bye for now."
+        else do
+            putStrLn $ readExpr input
+            main
 
-parseExpr :: Parser LispValue
-parseExpr = parseAtom
-            <|> parseString
-            <|> parseNumber
-
-parseString :: Parser LispValue
-parseString = do
-    char '"'
-    x <- many (noneOf "\"")
-    char '"'
-    return $ String x
-
-parseNumber :: Parser LispValue
-parseNumber = Number . read <$> many1 digit
-
-parseAtom :: Parser LispValue
-parseAtom = do
-    first <- letter <|> symbol
-    rest <- many (letter <|> digit <|> symbol)
-    let atom = first : rest
-    return $ case atom of
-        "#t" -> Bool True
-        "#f" -> Bool False
-        _    -> Atom atom
+readExpr :: String -> String
+readExpr input =
+    case parse (spaces >> symbol) "user_input" input of
+        Left error -> "No Match: " ++ show error
+        Right value -> "Found Value"
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
 spaces :: Parser ()
 spaces = skipMany1 space
-
-readExpr :: String -> String
-readExpr input = 
-    case parse parseExpr "user-input" input of
-        Left e -> "No Match: " ++ show e
-        Right v -> "Value Found: " ++ show v
-
-main :: IO ()
-main = putStr "wyos=> " 
-    >> getLine 
-    >>= putStrLn . readExpr
